a20810fbc278947004947afe24fb8a7ea848b585
Alex Krolewski <alex@krolewski.com>
2020-02-16 12:04:17 -0800

    fixed output column convention in get_b_dndz

--- get_b_dndz.py
+++ get_b_dndz.py
@@ -19,19 +19,20 @@ cli.add_argument("--dz",default=0.05,type=float,help="delta z")
 cli.add_argument("--boot_name",help="type of bootstrap error (literal, sqrt, marked)")
 cli.add_argument("--jack_name",help="type of jackknife error (loo, l2o)")
 cli.add_argument("--njack",type=int,help="number of jackknife resamples")
+cli.add_argument("--lower_ind",type=int,help="index of the minimum r_p bin to use. In the default binning scheme, 6: r_p,min=1.59 h^-1 Mpc, 7: r_p,min=2.52 h^-1 Mpc, 8: r_p,min=4 h^-1 Mpc")
+cli.add_argument("--nbins",type=int,help="number of bins in the outpu")
+
 
 ns = cli.parse_args()
+
+nbins = ns.nbins
+inds = ns.lower_ind
 
 
 t0 = time.time()
 plt.ion()
 plt.figure()
 
-#error = sys.argv[1]
-#name = sys.argv[2]
-#hemi = sys.argv[3]
-
-nbins = 15
 
 zs = np.linspace(ns.zmin,ns.zmax,int(round((ns.zmax-ns.zmin)/ns.dz))+1)
 
@@ -41,10 +42,7 @@ dndzall_err = np.zeros(len(zs)-1)
 nboot = int(ns.gal_name.split('_')[-1])
 
 for i in range(len(zs)-1):
-	#i = 3
-	#i = 5
 	try:
-		#dat = np.loadtxt('xcorr_out/unwise_DR14_QSO/ak/%s/nostar_%s/z%.2f_%.2f.txt' % (hemi,name,zs[i],zs[i+1]))
 		if ns.error == 'bootstrap':
 			ind = 'bs'
 			dat = np.loadtxt('%s/%s/z%.2f_%.2f_%s_%s.txt' % (ns.xcorrdir_name,ns.gal_name,zs[i],zs[i+1],ind,ns.boot_name))
@@ -57,30 +55,26 @@ for i in range(len(zs)-1):
 					nspec = float(line[9:])
 				
 			f.close()
-		#print time.time()-t0
 		s = dat[:,3]
-		#wsamples = dat[:,11:]
-		#cov = np.cov(wsamples)
-		#w = np.mean(wsamples,axis=1)
 		w = dat[:,6]
 		ds = dat[:,5]-dat[:,4] # will replace with ds from actual bins...
-		ind = np.where(s > 0.4)[0][0]
 		dndz = np.sum(ds[ind:]/s[ind:]*w[ind:])
-		#print time.time()-t0
 		err_poisson = dat[:,7]
-		cov = dat[:,10:10+nbins]
-		wjack_loo = dat[:,10+nbins:10+nbins+ns.njack]
-		cnts = dat[0,10+nbins+ns.njack:10+nbins+2*ns.njack]
+		#cov = dat[:,11:11+nbins]
+		cov = dat[:,13:13+nbins]
+		wjack_loo = dat[:,13+nbins:13+nbins+ns.njack]
+		cnts = dat[0,13+nbins+2*ns.njack:13+nbins+3*ns.njack]
+		
+		print cnts
 		print np.sqrt(np.diag(cov[4:,4:]))
 		
 		print nspec, ns.njack, i
-		
+	
 		if (np.any(cov[ind:,ind:] == 0)) or (nspec < ns.njack):
 			dndzall[i] = np.nan
 			dndzall_err[i] = np.nan
 		else:
 			wbar = np.sum((ds[ind:]/s[ind:])[:,np.newaxis]*wjack_loo[ind:,:],axis=0)
-			#dndz_err = np.sqrt(np.sum(np.dot(cov[ind:,ind:],ds[ind:]**2/s[ind:]**2)))
 			dndz_err = np.sqrt(np.sum((np.sum(cnts)-cnts)/np.sum(cnts) *(wbar-np.mean(wbar))**2.))	
 			print i, dndz_err, np.sqrt(np.sum(np.dot(cov[ind:,ind:],ds[ind:]**2/s[ind:]**2)))		
 			dndzall[i] = dndz
@@ -95,6 +89,11 @@ if ns.gal_name.find('/') != -1:
 	gal_name = ns.gal_name.replace('/','_')
 	
 #np.savetxt('bdndz/unwise_DR14_QSO/ak/%s/dz0.05/%s/nostar_%s.txt' % (hemi,error,name), np.array([dndzall,dndzall_err]).transpose())
+os.system('mkdir %s' % ('/'.join(ns.bdndz_name.split('/')[:-3])))
+os.system('mkdir %s' % ('/'.join(ns.bdndz_name.split('/')[:-2])))
+os.system('mkdir %s' % ('/'.join(ns.bdndz_name.split('/')[:-1])))
+os.system('mkdir %s' % (ns.bdndz_name))
+print 'mkdir %s/dz%.2f/' % (ns.bdndz_name, ns.dz)
 os.system('mkdir %s/dz%.2f/' % (ns.bdndz_name, ns.dz))
 os.system('mkdir %s/dz%.2f/%s' % (ns.bdndz_name,ns.dz,ns.error))
 if ns.error == 'bootstrap':
